c8547318643b0df13d936a6a35e2a66496eb1f4a
Brian Granger <ellisonbg@gmail.com>
2010-05-18 14:46:26 -0700

    Adding tests for large message bug.

--- zmq/tests/test_reqrep.py
+++ zmq/tests/test_reqrep.py
@@ -67,3 +67,11 @@ class TestReqRep(BaseZMQTestCase):
         o = dict(a=10,b=range(10))
         o2 = self.ping_pong_pyobj(s1, s2, o)
 
+    def test_large_msg(self):
+        s1, s2 = self.create_bound_pair(zmq.REQ, zmq.REP)
+        msg1 = 10000*'X'
+
+        for i in range(10):
+            msg2 = self.ping_pong(s1, s2, msg1)
+            self.assertEquals(msg1, msg2)
+
