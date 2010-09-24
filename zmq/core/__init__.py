ef9b90b9b1dbc91b31995fcb6d7acd48b287c0e7
MinRK <benjaminrk@gmail.com>
2010-09-23 19:36:46 -0700

    copied __init__.py

---
+++ zmq/core/__init__.py
@@ -0,0 +1,40 @@
+"""Python bindings for 0MQ."""
+
+#
+#    Copyright (c) 2010 Brian E. Granger
+#
+#    This file is part of pyzmq.
+#
+#    pyzmq is free software; you can redistribute it and/or modify it under
+#    the terms of the Lesser GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pyzmq is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    Lesser GNU General Public License for more details.
+#
+#    You should have received a copy of the Lesser GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+
+import os
+
+from zmq import _zmq
+from zmq._zmq import *
+
+
+__version__ = '2.0.9dev'
+
+
+def get_includes():
+    return os.path.dirname(__file__)
+
+
+__all__ = _zmq.__all__ + ['get_includes']
+
