db9b888fc74c1fceaddb034713f7993f8567bf17
MinRK <benjaminrk@gmail.com>
2010-11-16 21:00:02 -0800

    init 2.0.x branch

--- setup.py
+++ setup.py
@@ -254,7 +254,7 @@ the ZeroMQ library (http://www.zeromq.org).
 
 setup(
     name = "pyzmq",
-    version = "2.0.10",
+    version = "2.0dev",
     packages = ['zmq', 'zmq.tests', 'zmq.eventloop', 'zmq.log', 'zmq.core',
                 'zmq.devices', 'zmq.utils'],
     ext_modules = extensions,
