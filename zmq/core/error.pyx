b82c5a24e4a288ec9a0f9015a5b677a680673dda
MinRK <benjaminrk@gmail.com>
2011-06-15 15:47:59 -0700

    remove duplicated line

--- zmq/core/error.pyx
+++ zmq/core/error.pyx
@@ -76,7 +76,6 @@ class ZMQError(ZMQBaseError):
         if error is None:
             with nogil:
                 errno = zmq_errno()
-                errno = zmq_errno()
             error = errno
         if type(error) == int:
             self.strerror = strerror(error)
