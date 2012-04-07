18878a20c7d91ea57a65557e268c3e6596a4d2fe
MinRK <benjaminrk@gmail.com>
2012-04-07 16:09:38 -0700

    bump dev version to 2.2dev

--- zmq/core/version.py
+++ zmq/core/version.py
@@ -29,7 +29,7 @@ from zmq.core._version import zmq_version_info
 # Code
 #-----------------------------------------------------------------------------
 
-__version__ = '2.1dev'
+__version__ = '2.2dev'
 __revision__ = ''
 
 def pyzmq_version():
