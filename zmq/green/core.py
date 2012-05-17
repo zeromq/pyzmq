4afa8409de79dcc8cd655959611b52e8e79b6bfb
Michel Pelletier <pelletier.michel@yahoo.com>
2012-05-17 11:50:59 -0700

    Copied patch from Pawel Subocz to add poll support to zmq.green.

--- zmq/green/core.py
+++ zmq/green/core.py
@@ -17,7 +17,11 @@ from zmq import *
 # imported with different names as to not have the star import try to to clobber (when building with cython)
 from zmq.core.context import Context as _original_Context
 from zmq.core.socket import Socket as _original_Socket
+from zmq.core.poll import Poller as _original_Poller
 
+import gevent
+import gevent.core
+import gevent.select
 from gevent.event import AsyncResult
 from gevent.hub import get_hub
 
@@ -126,3 +130,81 @@ class _Socket(_original_Socket):
                 if e.errno != zmq.EAGAIN:
                     raise
             self._wait_read()
+
+
+class _Poller(_original_Poller):
+    """Replacement for :class:`zmq.core.Poller`
+
+    Ensures that the greened Poller below is used in calls to :meth:`zmq.core.Poller.poll`.
+    """
+
+    def _get_descriptors(self):
+        """Returns three elements tuple with socket descriptors ready for gevent.select.select
+        """
+        rlist = []
+        wlist = []
+        xlist = []
+
+        for socket, flags in self.sockets.items():
+            if isinstance(socket, _Socket):
+                fd = socket.getsockopt(FD)
+            elif isinstance(socket, int):
+                fd = socket
+            elif hasattr(socket, 'fileno'):
+                try:
+                    fd = int(socket.fileno())
+                except:
+                    raise ValueError('fileno() must return an valid integer fd')
+            else:
+                raise TypeError("Socket must be a 0MQ socket, an integer fd or have a fileno() method: %r" % socket)
+
+            if flags & POLLIN: rlist.append(fd)
+            if flags & POLLOUT: wlist.append(fd)
+            if flags & POLLERR: xlist.append(fd)
+
+        return (rlist, wlist, xlist)
+
+    def poll(self, timeout=-1):
+        """Overridden method to ensure that the green version of Poller is used
+
+        Behaves the same as :meth:`zmq.core.Poller.poll`
+        """
+
+        if timeout is None:
+            timeout = -1
+
+        timeout = int(timeout)
+        if timeout < 0:
+            timeout = -1
+
+        rlist = None
+        wlist = None
+        xlist = None
+
+        if timeout > 0:
+            tout = gevent.Timeout.start_new(timeout/1000.0)
+
+        try:
+            # Loop until timeout or events available
+            while True:
+                events = super(_Poller, self).poll(0)
+                if events or timeout == 0:
+                    return events
+
+                # wait for activity on sockets in a green way
+                if not rlist and not wlist and not xlist:
+                    rlist, wlist, xlist = self._get_descriptors()
+
+                try:
+                    gevent.select.select(rlist, wlist, xlist)
+                except gevent.select.error, ex:
+                    raise ZMQError(*ex.args)
+
+        except gevent.Timeout, t:
+            if t is not tout:
+                raise
+            return []
+        finally:
+           if timeout > 0:
+               tout.cancel()
+
