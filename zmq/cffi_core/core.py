4a98c003978496dc3c376401f9f12251a1164b26
Felipe cruz <felipecruz@loogica.net>
2012-10-30 22:12:46 -0200

    plug some high level pyzmq code

--- zmq/cffi_core/core.py
+++ zmq/cffi_core/core.py
@@ -258,3 +258,215 @@ class Socket(object):
         C.zmq_msg_close(zmq_msg)
 
         return value
+
+    def send_multipart(self, msg_parts, flags=0, copy=True, track=False):
+        """s.send_multipart(msg_parts, flags=0, copy=True, track=False)
+
+        Send a sequence of buffers as a multipart message.
+
+        Parameters
+        ----------
+        msg_parts : iterable
+            A sequence of objects to send as a multipart message. Each element
+            can be any sendable object (Frame, bytes, buffer-providers)
+        flags : int, optional
+            SNDMORE is handled automatically for frames before the last.
+        copy : bool, optional
+            Should the frame(s) be sent in a copying or non-copying manner.
+        track : bool, optional
+            Should the frame(s) be tracked for notification that ZMQ has
+            finished with it (ignored if copy=True).
+
+        Returns
+        -------
+        None : if copy or not track
+        MessageTracker : if track and not copy
+            a MessageTracker object, whose `pending` property will
+            be True until the last send is completed.
+        """
+        for msg in msg_parts[:-1]:
+            self.send(msg, SNDMORE|flags, copy=copy, track=track)
+        # Send the last part without the extra SNDMORE flag.
+        return self.send(msg_parts[-1], flags, copy=copy, track=track)
+
+    def recv_multipart(self, flags=0, copy=True, track=False):
+        """s.recv_multipart(flags=0, copy=True, track=False)
+
+        Receive a multipart message as a list of bytes or Frame objects.
+
+        Parameters
+        ----------
+        flags : int, optional
+            Any supported flag: NOBLOCK. If NOBLOCK is set, this method
+            will raise a ZMQError with EAGAIN if a message is not ready.
+            If NOBLOCK is not set, then this method will block until a
+            message arrives.
+        copy : bool, optional
+            Should the message frame(s) be received in a copying or non-copying manner?
+            If False a Frame object is returned for each part, if True a copy of
+            the bytes is made for each frame.
+        track : bool, optional
+            Should the message frame(s) be tracked for notification that ZMQ has
+            finished with it? (ignored if copy=True)
+        Returns
+        -------
+        msg_parts : list
+            A list of frames in the multipart message; either Frames or bytes,
+            depending on `copy`.
+
+        """
+        parts = [self.recv(flags, copy=copy, track=track)]
+        # have first part already, only loop while more to receive
+        while self.getsockopt(zmq.RCVMORE):
+            part = self.recv(flags, copy=copy, track=track)
+            parts.append(part)
+
+        return parts
+
+    def send_string(self, u, flags=0, copy=False, encoding='utf-8'):
+        """s.send_string(u, flags=0, copy=False, encoding='utf-8')
+
+        Send a Python unicode string as a message with an encoding.
+
+        0MQ communicates with raw bytes, so you must encode/decode
+        text (unicode on py2, str on py3) around 0MQ.
+
+        Parameters
+        ----------
+        u : Python unicode string (unicode on py2, str on py3)
+            The unicode string to send.
+        flags : int, optional
+            Any valid send flag.
+        encoding : str [default: 'utf-8']
+            The encoding to be used
+        """
+        if not isinstance(u, basestring):
+            raise TypeError("unicode/str objects only")
+        return self.send(u.encode(encoding), flags=flags, copy=copy)
+
+    def recv_string(self, flags=0, encoding='utf-8'):
+        """s.recv_string(flags=0, encoding='utf-8')
+
+        Receive a unicode string, as sent by send_string.
+
+        Parameters
+        ----------
+        flags : int
+            Any valid recv flag.
+        encoding : str [default: 'utf-8']
+            The encoding to be used
+
+        Returns
+        -------
+        s : unicode string (unicode on py2, str on py3)
+            The Python unicode string that arrives as encoded bytes.
+        """
+        msg = self.recv(flags=flags, copy=False)
+        return codecs.decode(msg.bytes, encoding)
+
+
+        """s.send_pyobj(obj, flags=0, protocol=-1)
+
+        Send a Python object as a message using pickle to serialize.
+
+        Parameters
+        ----------
+        obj : Python object
+            The Python object to send.
+        flags : int
+            Any valid send flag.
+        protocol : int
+            The pickle protocol number to use. Default of -1 will select
+            the highest supported number. Use 0 for multiple platform
+            support.
+        """
+        msg = pickle.dumps(obj, protocol)
+        return self.send(msg, flags)
+
+    def recv_pyobj(self, flags=0):
+        """s.recv_pyobj(flags=0)
+
+        Receive a Python object as a message using pickle to serialize.
+
+        Parameters
+        ----------
+        flags : int
+            Any valid recv flag.
+
+        Returns
+        -------
+        obj : Python object
+            The Python object that arrives as a message.
+        """
+        s = self.recv(flags)
+        return pickle.loads(s)
+
+    def send_json(self, obj, flags=0):
+        """s.send_json(obj, flags=0)
+
+        Send a Python object as a message using json to serialize.
+
+        Parameters
+        ----------
+        obj : Python object
+            The Python object to send.
+        flags : int
+            Any valid send flag.
+        """
+        if jsonapi.jsonmod is None:
+            raise ImportError('jsonlib{1,2}, json or simplejson library is required.')
+        else:
+            msg = jsonapi.dumps(obj)
+            return self.send(msg, flags)
+
+    def recv_json(self, flags=0):
+        """s.recv_json(flags=0)
+
+        Receive a Python object as a message using json to serialize.
+
+        Parameters
+        ----------
+        flags : int
+            Any valid recv flag.
+
+        Returns
+        -------
+        obj : Python object
+            The Python object that arrives as a message.
+        """
+        if jsonapi.jsonmod is None:
+            raise ImportError('jsonlib{1,2}, json or simplejson library is required.')
+        else:
+            msg = self.recv(flags)
+            return jsonapi.loads(msg)
+
+    def poll(self, timeout=None, flags=POLLIN):
+        """s.poll(timeout=None, flags=POLLIN)
+
+        Poll the socket for events.  The default is to poll forever for incoming
+        events.  Timeout is in milliseconds, if specified.
+
+        Parameters
+        ----------
+        timeout : int [default: None]
+            The timeout (in milliseconds) to wait for an event. If unspecified
+            (or secified None), will wait forever for an event.
+        flags : bitfield (int) [default: POLLIN]
+            The event flags to poll for (any combination of POLLIN|POLLOUT).
+            The default is to check for incoming events (POLLIN).
+
+        Returns
+        -------
+        events : bitfield (int)
+            The events that are ready and waiting.  Will be 0 if no events were ready
+            by the time timeout was reached.
+        """
+
+        if self.closed:
+            raise ZMQError(ENOTSUP)
+
+        p = zmq.Poller()
+        p.register(self, flags)
+        evts = dict(p.poll(timeout))
+        # return 0 if no events, otherwise return event bitfield
+        return evts.get(self, 0)
