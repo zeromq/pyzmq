d99571ce5c11f0577687c74410f4802178cee3a4
Felipe cruz <felipecruz@loogica.net>
2012-11-19 00:11:13 -0200

    update low-level C decls and add IPC_MAX_PATH_LEN
    constant and check function

--- zmq/cffi_core/_cffi.py
+++ zmq/cffi_core/_cffi.py
@@ -2,7 +2,53 @@
 
 from cffi import FFI
 
-ffi = FFI()
+class PythonFFI(FFI):
+
+    def __init__(self, backend=None):
+        FFI.__init__(self, backend=backend)
+        self._pyexports = {}
+
+    def pyexport(self, signature):
+        tp = self._typeof(signature, consider_function_as_funcptr=True)
+        def decorator(func):
+            name = func.__name__
+            if name in self._pyexports:
+                raise cffi.CDefError("duplicate pyexport'ed function %r"
+                                     % (name,))
+            callback_var = self.getctype(tp, name)
+            self.cdef("%s;" % callback_var)
+            self._pyexports[name] = _PyExport(tp, func)
+        return decorator
+
+    def verify(self, source='', **kwargs):
+        extras = []
+        pyexports = sorted(self._pyexports.items())
+        for name, export in pyexports:
+            callback_var = self.getctype(export.tp, name)
+            extras.append("%s;" % callback_var)
+        extras.append(source)
+        source = '\n'.join(extras)
+        lib = FFI.verify(self, source, **kwargs)
+        for name, export in pyexports:
+            cb = self.callback(export.tp, export.func)
+            export.cb = cb
+            setattr(lib, name, cb)
+        return lib
+
+
+class _PyExport(object):
+    def __init__(self, tp, func):
+        self.tp = tp
+        self.func = func
+
+hints = []
+
+ffi = PythonFFI()
+
+@ffi.pyexport("void(char*, int)")
+def python_free_callback(data, hint_index):
+    print(hints[hint_index])
+
 
 core_functions = \
 '''
@@ -51,6 +97,11 @@ int zmq_msg_close(zmq_msg_t *msg);
 
 int zmq_send(void *socket, zmq_msg_t *msg, int flags);
 int zmq_recv(void *socket, zmq_msg_t *msg, int flags);
+
+int custom_zmq_msg_init(zmq_msg_t *message,
+                        void *data,
+                        size_t size,
+                        void *hint);
 '''
 
 message32_functions = \
@@ -72,6 +123,11 @@ int zmq_msg_close(zmq_msg_t *msg);
 
 int zmq_sendmsg(void *socket, zmq_msg_t *msg, int flags);
 int zmq_recvmsg(void *socket, zmq_msg_t *msg, int flags);
+
+int custom_zmq_msg_init(zmq_msg_t *message,
+                        void *data,
+                        size_t size,
+                        void *hint);
 '''
 
 sockopt_functions = \
@@ -103,6 +159,8 @@ int zmq_poll(zmq_pollitem_t *items, int nitems, long timeout);
 extra_functions = \
 '''
 char* strncpy(char* dest, char* orig, size_t len);
+void * memcpy(void *restrict s1, const void *restrict s2, size_t n);
+int get_ipc_path_max_len(void);
 '''
 
 def zmq_version_info():
@@ -147,6 +205,26 @@ C = ffi.verify('''
     #include <string.h>
     #include <zmq.h>
     #include <stdio.h>
+    #include <sys/un.h>
+
+void pyzmq_cffi_free(void *data, void* hint) {
+    python_free_callback((char*)data, (int) hint);
+}
+
+int custom_zmq_msg_init(zmq_msg_t *message,
+                        void *data,
+                        size_t size,
+                        void *hint)
+{
+    int rc = zmq_msg_init_data(message, data, size, pyzmq_cffi_free, hint);
+    return rc;
+}
+
+int get_ipc_path_max_len(void) {
+    struct sockaddr_un *dummy;
+    return sizeof(dummy->sun_path) - 1;
+}
+
 ''', libraries=['c', 'zmq'])
 
 nsp = new_sizet_pointer = lambda length: ffi.new('size_t*', length)
@@ -171,3 +249,5 @@ value_binary_data = lambda val, length: (ffi.new('char[%d]' % (length), val),
 
 def strerror(errnum):
     return ffi.string(C.strerror(errnum))
+
+IPC_PATH_MAX_LEN = C.get_ipc_path_max_len()
