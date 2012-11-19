c6b393b41d954442fd9a190cc4eff06a05cb9825
Felipe cruz <felipecruz@loogica.net>
2012-11-19 00:07:32 -0200

    accept msg keyword param on ZMQError

--- zmq/cffi_core/error.py
+++ zmq/cffi_core/error.py
@@ -1,11 +1,13 @@
 from ._cffi import C, strerror
 
 class ZMQError(Exception):
-    def __init__(self, errno):
+    def __init__(self, errno, msg=None):
         if not errno:
             self.errno = C.zmq_errno()
         else:
             self.errno = errno
+        if msg:
+            self.strerror = msg
 
     def __str__(self):
         return strerror(self.errno)
