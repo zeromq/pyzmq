92a8fc79d1450199e1df81b3e808d2031b550b63
MinRK <benjaminrk@gmail.com>
2013-01-30 20:09:27 -0800

    test new exceptions
    poke @travisbot

--- zmq/tests/test_error.py
+++ zmq/tests/test_error.py
@@ -15,14 +15,14 @@
 import sys
 import time
 
-from unittest import TestCase
-
-from zmq import ZMQError, strerror
+import zmq
+from zmq import ZMQError, strerror, Again, ContextTerminated
+from zmq.tests import BaseZMQTestCase
 
 if sys.version_info[0] >= 3:
     long = int
 
-class TestZMQError(TestCase):
+class TestZMQError(BaseZMQTestCase):
     
     def test_strerror(self):
         """test that strerror gets the right type."""
@@ -35,4 +35,19 @@ class TestZMQError(TestCase):
             e = ZMQError(errno)
             self.assertEqual(e.errno, errno)
             self.assertEqual(str(e), strerror(errno))
+    
+    def test_again(self):
+        s = self.context.socket(zmq.REP)
+        self.assertRaises(Again, s.recv, zmq.NOBLOCK)
+        self.assertRaisesErrno(zmq.EAGAIN, s.recv, zmq.NOBLOCK)
+        s.close()
+    
+    def atest_ctxterm(self):
+        s = self.context.socket(zmq.REP)
+        t = Thread(target=self.context.term)
+        t.start()
+        self.assertRaises(ContextTerminated, s.recv, zmq.NOBLOCK)
+        self.assertRaisesErrno(zmq.TERM, s.recv, zmq.NOBLOCK)
+        s.close()
+        t.join()
 
