be78aae4ec52a56ecf2c8606a1e4c980aa4ab391
MinRK <benjaminrk@gmail.com>
2013-02-01 21:13:28 -0800

    use zmq.proxy in zmq.device

--- zmq/cffi_core/devices.py
+++ zmq/cffi_core/devices.py
@@ -5,7 +5,7 @@ from .socket import Socket
 from zmq.error import ZMQError
 
 def device(device_type, frontend, backend):
-    rc = C.zmq_device(device_type, frontend._zmq_socket, backend._zmq_socket)
+    rc = C.zmq_proxy(frontend._zmq_socket, backend._zmq_socket, ffi.NULL)
 
     if rc != 0:
         raise ZMQError(C.zmq_errno())
