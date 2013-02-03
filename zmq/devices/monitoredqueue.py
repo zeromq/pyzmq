9077a88b4e9d09026700bdf9dc18ede85f0a2091
MinRK <benjaminrk@gmail.com>
2013-02-03 11:32:19 -0800

    add header / newline to Python MQ

--- zmq/devices/monitoredqueue.py
+++ zmq/devices/monitoredqueue.py
@@ -1,5 +1,22 @@
-import zmq
+"""pure Python monitored_queue function
 
+For use when Cython extension is unavailable (PyPy).
+
+Authors
+-------
+* MinRK
+"""
+
+#-----------------------------------------------------------------------------
+#  Copyright (c) 2010-2012 Brian Granger, Min Ragan-Kelley
+#
+#  This file is part of pyzmq
+#
+#  Distributed under the terms of the New BSD License.  The full license is in
+#  the file COPYING.BSD, distributed as part of this software.
+#-----------------------------------------------------------------------------
+
+import zmq
 
 def _relay(ins, outs, sides, prefix, swap_ids):
     msg = ins.recv_multipart()
