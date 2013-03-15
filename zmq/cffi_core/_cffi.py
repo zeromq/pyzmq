db437d8724fee18f6e569460409d366f0ca6b22c
Felipe cruz <felipecruz@loogica.net>
2013-03-15 11:42:41 -0300

    remove unnecessary code

--- zmq/cffi_core/_cffi.py
+++ zmq/cffi_core/_cffi.py
@@ -12,52 +12,7 @@
 
 from cffi import FFI
 
-try:
-    # below 3.3
-    from threading import _Event as Event
-except (ImportError):
-    from threading import Event
-
-class PythonFFI(FFI):
-
-    def __init__(self, backend=None):
-        FFI.__init__(self, backend=backend)
-        self._pyexports = {}
-
-    def pyexport(self, signature):
-        tp = self._typeof(signature, consider_function_as_funcptr=True)
-        def decorator(func):
-            name = func.__name__
-            if name in self._pyexports:
-                raise cffi.CDefError("duplicate pyexport'ed function %r"
-                                     % (name,))
-            callback_var = self.getctype(tp, name)
-            self.cdef("%s;" % callback_var)
-            self._pyexports[name] = _PyExport(tp, func)
-        return decorator
-
-    def verify(self, source='', **kwargs):
-        extras = []
-        pyexports = sorted(self._pyexports.items())
-        for name, export in pyexports:
-            callback_var = self.getctype(export.tp, name)
-            extras.append("%s;" % callback_var)
-        extras.append(source)
-        source = '\n'.join(extras)
-        lib = FFI.verify(self, source, **kwargs)
-        for name, export in pyexports:
-            cb = self.callback(export.tp, export.func)
-            export.cb = cb
-            setattr(lib, name, cb)
-        return lib
-
-
-class _PyExport(object):
-    def __init__(self, tp, func):
-        self.tp = tp
-        self.func = func
-
-ffi = PythonFFI()
+ffi = FFI()
 
 core_functions = \
 '''
