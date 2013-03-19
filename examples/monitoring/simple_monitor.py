40dfaf4e52c06e79395f962be0c25b1bc68211e0
Guido Goldstein <github@a-nugget.de>
2013-03-19 09:59:42 +0100

    Fixed licencese.

--- examples/monitoring/simple_monitor.py
+++ examples/monitoring/simple_monitor.py
@@ -6,7 +6,7 @@ __license__ = '''
     This file is part of pyzmq.
 
     pyzmq is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
+    it under the terms of the Lesser GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
