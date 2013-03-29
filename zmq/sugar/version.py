9919a0fbede69d26aa4c50288506dd92faecdfdb
MinRK <benjaminrk@gmail.com>
2013-03-29 14:55:36 -0700

    back to dev

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -21,7 +21,7 @@ from .backend import zmq_version_info
 # Code
 #-----------------------------------------------------------------------------
 
-__version__ = '13.0.2'
+__version__ = '13.0.dev'
 __revision__ = ''
 
 def pyzmq_version():
