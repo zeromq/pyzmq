302ade40465c7e78a6ad2b8fcb4c355ab9d5eb23
Phus Lu <phus.lu@gmail.com>
2013-09-02 19:10:58 +0800

    use setuptools to dynamic load freeze pyzmq egg
    use pkg_resources to load libzmq.{so,pyd}, which we could package pyzmq as a single egg file. it will make us we could deploy pyzmq on the fly.

--- zmq/__init__.py
+++ zmq/__init__.py
@@ -34,6 +34,17 @@ if bundled:
     else:
         _libzmq = ctypes.CDLL(bundled[0], mode=ctypes.RTLD_GLOBAL)
     del ctypes
+else:
+    import imp
+    pkg_resources = None
+    try:
+        import pkg_resources
+        ext = next(ext for ext, _, _type in imp.get_suffixes() if _type == 3)
+        imp.load_dynamic('zmq.libzmq', pkg_resources.resource_filename('zmq.libzmq', 'libzmq'+ext))
+    except ImportError as e:
+        pass
+    finally:
+        del imp, pkg_resources
 
 # init Python threads
 
