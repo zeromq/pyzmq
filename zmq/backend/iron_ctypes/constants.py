64dc3242280b58dff938ef93fe38109cc9642f9e
Pawel Jasinski <pawel.jasinski@gmail.com>
2014-01-13 15:11:16 +0100

    added h2py, added comments about errno

--- zmq/backend/iron_ctypes/constants.py
+++ zmq/backend/iron_ctypes/constants.py
@@ -12,11 +12,8 @@
 
 from zmq.utils.constant_names import all_names
 
-# ZMQ.py is generated from zmq.h using h2py.py
+# ZMQ.py is generated from zmq.h during setup h2py.py
 import ZMQ
-
-from ._version import zmq_version_info
-
 g = globals()
 for cname, cvalue in ZMQ.__dict__.iteritems():
     if cname.startswith("__"):
@@ -26,15 +23,24 @@ for cname, cvalue in ZMQ.__dict__.iteritems():
     g[cname] = cvalue
 
 # ERRNO.py is generated out of system errno.h using h2py.py
-# values in ERRNO overwrite values in ZMQ.py
+#
+# Standard python errno module (Ironpython 2.7.4) cannot be used, because
+# ironpython uses WSA values which do not match posix values. For example:
+# errno.EADDRINUSE in ironpython is 10048 which is WSAEADDRINUSE
+# errno.EADDRINUSE returned by libzmq is 100
+
 from .ERRNO import *
 
+# extract the actual library version
+# TODO: cross check with version of the header used by setup
+#       issue warning if mismatch
+from ._version import zmq_version_info
 VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH = zmq_version_info()
 VERSION = "%d%02d%02d" % zmq_version_info()
 
+# everything what should have name but has no value, should have value -1
 for name in all_names:
     if not name in g:
         g[name] = -1
-
 
 __all__ = all_names
