f7f87f267bec59afba233a6bfb061bd0d4978660
Alexander Pyhalov <apyhalov@gmail.com>
2014-01-23 09:21:25 +0400

    Allow linking 64-bit executables on Solaris-derived i86 platforms

--- buildutils/detect.py
+++ buildutils/detect.py
@@ -56,6 +56,11 @@ def test_compilation(cfile, compiler=None, **compiler_attrs):
         else:
             # allow for missing UB arch, since it will still work:
             lpreargs = ['-undefined', 'dynamic_lookup']
+    if sys.platform == 'sunos5':
+        if platform.architecture()[0]=='32bit':
+            lpreargs = ['-m32']
+        else: 
+            lpreargs = ['-m64']
     extra = compiler_attrs.get('extra_compile_args', None)
 
     objs = cc.compile([cfile],extra_preargs=cpreargs, extra_postargs=extra)
