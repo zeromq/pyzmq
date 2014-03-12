2b797879f3a4bf148575df1309369ed4532e4ab6
MinRK <benjaminrk@gmail.com>
2014-03-11 21:02:06 -0700

    bump version to 14.1.0

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -22,7 +22,7 @@ from zmq.backend import zmq_version_info
 VERSION_MAJOR = 14
 VERSION_MINOR = 1
 VERSION_PATCH = 0
-VERSION_EXTRA = 'dev'
+VERSION_EXTRA = ''
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 if VERSION_EXTRA:
