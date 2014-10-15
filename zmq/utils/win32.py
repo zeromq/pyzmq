919d10cc9ee295b1184eef4cbc90c9893101a1ba
MinRK <benjaminrk@gmail.com>
2014-10-15 13:41:43 -0700

    put docstrings in base _allow_interrupt
    allows autodoc to find it on Non-Windows

--- zmq/utils/win32.py
+++ zmq/utils/win32.py
@@ -7,6 +7,78 @@
 
 import os
 
+# No-op implementation for other platforms.
+class _allow_interrupt(object):
+    """Utility for fixing CTRL-C events on Windows.
+
+    On Windows, the Python interpreter intercepts CTRL-C events in order to
+    translate them into ``KeyboardInterrupt`` exceptions.  It (presumably)
+    does this by setting a flag in its "control control handler" and
+    checking it later at a convenient location in the interpreter.
+
+    However, when the Python interpreter is blocked waiting for the ZMQ
+    poll operation to complete, it must wait for ZMQ's ``select()``
+    operation to complete before translating the CTRL-C event into the
+    ``KeyboardInterrupt`` exception.
+
+    The only way to fix this seems to be to add our own "console control
+    handler" and perform some application-defined operation that will
+    unblock the ZMQ polling operation in order to force ZMQ to pass control
+    back to the Python interpreter.
+
+    This context manager performs all that Windows-y stuff, providing you
+    with a hook that is called when a CTRL-C event is intercepted.  This
+    hook allows you to unblock your ZMQ poll operation immediately, which
+    will then result in the expected ``KeyboardInterrupt`` exception.
+
+    Without this context manager, your ZMQ-based application will not
+    respond normally to CTRL-C events on Windows.  If a CTRL-C event occurs
+    while blocked on ZMQ socket polling, the translation to a
+    ``KeyboardInterrupt`` exception will be delayed until the I/O completes
+    and control returns to the Python interpreter (this may never happen if
+    you use an infinite timeout).
+
+    A no-op implementation is provided on non-Win32 systems to avoid the
+    application from having to conditionally use it.
+
+    Example usage:
+
+    .. sourcecode:: python
+
+       def stop_my_application():
+           # ...
+
+       with allow_interrupt(stop_my_application):
+           # main polling loop.
+
+    In a typical ZMQ application, you would use the "self pipe trick" to
+    send message to a ``PAIR`` socket in order to interrupt your blocking
+    socket polling operation.
+
+    In a Tornado event loop, you can use the ``IOLoop.stop`` method to
+    unblock your I/O loop.
+    """
+
+    def __init__(self, action=None):
+        """Translate ``action`` into a CTRL-C handler.
+
+        ``action`` is a callable that takes no arguments and returns no
+        value (returned value is ignored).  It must *NEVER* raise an
+        exception.
+        
+        If unspecified, a no-op will be used.
+        """
+        self._init_action(action)
+    
+    def _init_action(self, action):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return
+
 if os.name == 'nt':
     from ctypes import WINFUNCTYPE, windll
     from ctypes.wintypes import BOOL, DWORD
@@ -19,67 +91,12 @@ if os.name == 'nt':
     SetConsoleCtrlHandler.argtypes = (PHANDLER_ROUTINE, BOOL)
     SetConsoleCtrlHandler.restype = BOOL
 
-    class allow_interrupt(object):
-        """Utility for fixing CTRL-C events on Windows.
+    class allow_interrupt(_allow_interrupt):
 
-        On Windows, the Python interpreter intercepts CTRL-C events in order to
-        translate them into ``KeyboardInterrupt`` exceptions.  It (presumably)
-        does this by setting a flag in its "control control handler" and
-        checking it later at a convenient location in the interpreter.
-
-        However, when the Python interpreter is blocked waiting for the ZMQ
-        poll operation to complete, it must wait for ZMQ's ``select()``
-        operation to complete before translating the CTRL-C event into the
-        ``KeyboardInterrupt`` exception.
-
-        The only way to fix this seems to be to add our own "console control
-        handler" and perform some application-defined operation that will
-        unblock the ZMQ polling operation in order to force ZMQ to pass control
-        back to the Python interpreter.
-
-        This context manager performs all that Windows-y stuff, providing you
-        with a hook that is called when a CTRL-C event is intercepted.  This
-        hook allows you to unblock your ZMQ poll operation immediately, which
-        will then result in the expected ``KeyboardInterrupt`` exception.
-
-        Without this context manager, your ZMQ-based application will not
-        respond normally to CTRL-C events on Windows.  If a CTRL-C event occurs
-        while blocked on ZMQ socket polling, the translation to a
-        ``KeyboardInterrupt`` exception will be delayed until the I/O completes
-        and control returns to the Python interpreter (this may never happen if
-        you use an infinite timeout).
-
-        A no-op implementation is provided on non-Win32 systems to avoid the
-        application from having to conditionally use it.
-
-        Example usage:
-
-        .. sourcecode:: python
-
-           def stop_my_application():
-               # ...
-
-           with allow_interrupt(stop_my_application):
-               # main polling loop.
-
-        In a typical ZMQ application, you would use the "self pipe trick" to
-        send message to a ``PAIR`` socket in order to interrupt your blocking
-        socket polling operation.
-
-        In a Tornado event loop, you can use the ``IOLoop.stop`` method to
-        unblock your I/O loop.
-        """
-
-        def __init__(self, action=None):
-            """Translate ``action`` into a CTRL-C handler.
-
-            ``action`` is a callable that takes no arguments and returns no
-            value (returned value is ignored).  It must *NEVER* raise an
-            exception.
-            
-            If unspecified, a no-op will be used.
-            """
-            self.action = action or lambda : None
+        def init_action(self, action):
+            if action is None:
+                action = lambda: None
+            self.action = action
             @PHANDLER_ROUTINE
             def handle(event):
                 if event == 0:  # CTRL_C_EVENT
@@ -100,7 +117,6 @@ if os.name == 'nt':
                 # Have standard library automatically call `GetLastError()` and
                 # `FormatMessage()` into a nice exception object :-)
                 raise WindowsError()
-            return self
 
         def __exit__(self, *args):
             """Remove the custom CTRL-C handler."""
@@ -110,8 +126,6 @@ if os.name == 'nt':
                 # `FormatMessage()` into a nice exception object :-)
                 raise WindowsError()
 else:
-    # No-op implementation for other platforms.
-    from contextlib import contextmanager
-    @contextmanager
-    def allow_interrupt(action=None):
-        yield
+    class allow_interrupt(_allow_interrupt):
+        pass
+allow_interrupt.__doc__ = _allow_interrupt.__doc__
