e4cffc13352afdd3f0cebb3c1855ffd55d07efd0
Nicolas Delaby <nicolas.delaby@ezeep.com>
2015-02-15 16:40:01 +0100

    Test python3.4 and flag it as supported

--- setup.py
+++ setup.py
@@ -1180,6 +1180,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
 if 'setuptools' in sys.modules and pypy:
