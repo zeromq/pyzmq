89f8fe082121b6bc101aba37f4fc0cbf31808e14
Min RK <benjaminrk@gmail.com>
2015-02-25 14:41:11 -0800

    use :0 in bind_to_random_port
    when LAST_ENDPOINT is available (libzmq ≥ 3.x)
    for retrieving the result of the bind.

--- zmq/sugar/socket.py
+++ zmq/sugar/socket.py
@@ -185,6 +185,14 @@ class Socket(SocketBase, AttributeSetter):
         ZMQBindError
             if `max_tries` reached before successful bind
         """
+        if hasattr(constants, 'LAST_ENDPOINT') and min_port == 49152 and max_port == 65536:
+            # if LAST_ENDPOINT is supported, and min_port / max_port weren't specified,
+            # we can bind to port 0 and let the OS do the work
+            self.bind("%s:0" % addr)
+            url = self.last_endpoint.decode('ascii', 'replace')
+            _, port_s = url.rsplit(':', 1)
+            return int(port_s)
+        
         for i in range(max_tries):
             try:
                 port = random.randrange(min_port, max_port)
