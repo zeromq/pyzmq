fb9c02aace2301eb8cc2dc520c8efa4338735293
Min RK <benjaminrk@gmail.com>
2015-04-16 14:03:56 -0700

    fix `setup.py clean`

--- setup.py
+++ setup.py
@@ -833,51 +833,51 @@ class CleanCommand(Command):
         pass
 
     def run(self):
-        self._clean_me = []
-        self._clean_trees = []
+        _clean_me = []
+        _clean_trees = []
         
         for d in ('build', 'dist', 'conf'):
             if os.path.exists(d):
-                self._clean_trees.append(d)
+                _clean_trees.append(d)
         
         for root, dirs, files in os.walk('buildutils'):
-            if any(root.startswith(pre) for pre in self._clean_trees):
+            if any(root.startswith(pre) for pre in _clean_trees):
                 continue
             for f in files:
                 if os.path.splitext(f)[-1] == '.pyc':
-                    self._clean_me.append(pjoin(root, f))
+                    _clean_me.append(pjoin(root, f))
             
             if '__pycache__' in dirs:
-                self._clean_trees.append(pjoin(root, '__pycache__'))
+                _clean_trees.append(pjoin(root, '__pycache__'))
 
         for root, dirs, files in os.walk('zmq'):
-            if any(root.startswith(pre) for pre in self._clean_trees):
+            if any(root.startswith(pre) for pre in _clean_trees):
                 continue
             
             for f in files:
                 if os.path.splitext(f)[-1] in ('.pyc', '.so', '.o', '.pyd', '.json'):
-                    self._clean_me.append(pjoin(root, f))
+                    _clean_me.append(pjoin(root, f))
 
             for d in dirs:
                 if d == '__pycache__':
-                    self._clean_trees.append(pjoin(root, d))
+                    _clean_trees.append(pjoin(root, d))
         
         # remove generated cython files
         if self.all:
             for root, dirs, files in os.walk(pjoin('zmq', 'backend', 'cython')):
                  if os.path.splitext(f)[-1] == '.c':
-                     self._clean_me.append(pjoin(root, f))
+                     _clean_me.append(pjoin(root, f))
         
         bundled = glob(pjoin('zmq', 'libzmq*'))
-        self._clean_me.extend([ b for b in bundled if b not in _clean_me ])
+        _clean_me.extend([ b for b in bundled if b not in _clean_me ])
         
-        for clean_me in self._clean_me:
+        for clean_me in _clean_me:
             print("removing %s" % clean_me)
             try:
                 os.unlink(clean_me)
             except Exception as e:
                 print(e, file=sys.stderr)
-        for clean_tree in self._clean_trees:
+        for clean_tree in _clean_trees:
             print("removing %s/" % clean_tree)
             try:
                 shutil.rmtree(clean_tree)
