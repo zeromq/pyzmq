6d0f9b86cb7b5e8ec2baaa8fbaae87482147ae1f
Min RK <benjaminrk@gmail.com>
2015-04-23 14:49:03 -0700

    release 14.6.0

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -8,9 +8,9 @@ from zmq.backend import zmq_version_info
 
 
 VERSION_MAJOR = 14
-VERSION_MINOR = 5
-VERSION_PATCH = 1
-VERSION_EXTRA = "dev"
+VERSION_MINOR = 6
+VERSION_PATCH = 0
+VERSION_EXTRA = ""
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 if VERSION_EXTRA:
