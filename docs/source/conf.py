1d76aa8dfd79fc0a2b6e0dc68ebb5f2da408510e
Min RK <benjaminrk@gmail.com>
2015-06-14 16:46:28 -0700

    update sphinx config for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,10 +28,8 @@ import sphinx_cython
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
               'sphinx.ext.autodoc',
-              'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
-              'ipython_console_highlighting',
-              'numpydoc',
+              'sphinx.ext.napoleon',
               ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -106,7 +104,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+# html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
