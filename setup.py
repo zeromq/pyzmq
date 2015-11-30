cd82301b4590b2326d31fe57a636fa39254eb7f2
Min RK <benjaminrk@gmail.com>
2015-11-30 13:18:40 +0100

    old-style classes don't have super
    Seriously, Python2, you guys.

--- setup.py
+++ setup.py
@@ -1121,7 +1121,7 @@ else:
     class zbuild_ext(build_ext_c):
         
         def finalize_options(self):
-            super(zbuild_ext, self).finalize_options()
+            build_ext_c.finalize_options(self)
             # set binding so that compiled methods can be inspected
             self.cython_directives['binding'] = True
         
