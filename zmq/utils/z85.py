1652e36ac271b822fb0e3187a4e3660481ae9357
Sekenre <kio@mothers-arms.co.uk>
2016-03-04 11:10:14 +0000

    Raise TypeError instead of encoding

--- zmq/utils/z85.py
+++ zmq/utils/z85.py
@@ -44,7 +44,7 @@ def encode(rawbytes):
 def decode(z85bytes):
     """decode Z85 bytes to raw bytes"""
     if PY3 and isinstance(z85bytes, str):
-        z85bytes = z85bytes.encode('ascii')
+        raise TypeError("Unicode-objects must be encoded to bytes")
 
     if len(z85bytes) % 5:
         raise ValueError("Z85 length must be multiple of 5, not %i" % len(z85bytes))
