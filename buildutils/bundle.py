0044e6825400343b4c90c4eb2277755c8eceafc4
Min RK <benjaminrk@gmail.com>
2016-06-02 15:41:04 +0200

    bump bundled libzmq-4.1.5pre
    still no 4.1.5 release

--- buildutils/bundle.py
+++ buildutils/bundle.py
@@ -37,8 +37,8 @@ libzmq = "zeromq-%i.%i.%i.tar.gz" % (bundled_version)
 libzmq_url = "http://download.zeromq.org/" + libzmq
 # FIXME: temporarily use GitHub download while waiting for 4.1.5 release
 # use codeload URL because SSL fails on github.com/archive URLs on Windows for some reason
-libzmq_url = "https://codeload.github.com/zeromq/zeromq4-1/tar.gz/0b781f84e8a93796a3902bd5ec2828391e9e2bd1"
-libzmq_checksum = "sha256:01b8eeae47e928bddff97adc1706e33ad09165b456dc3782185673d3ac5a0bf4"
+libzmq_url = "https://codeload.github.com/zeromq/zeromq4-1/tar.gz/56b71af22db3232eb57fd72e954e22eeac497b46"
+libzmq_checksum = "sha256:f28898473569ac9119e1f01bae339cb5181bbffca2923a07b4e3a1b0229dbc01"
 
 HERE = os.path.dirname(__file__)
 ROOT = os.path.dirname(HERE)
