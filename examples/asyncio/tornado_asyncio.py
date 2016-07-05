e735affa509a652f8b2d6154f7a1692a04100f8c
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-07-05 17:04:54 -0400

    Added ZMQ asyncio sockets to example.

--- examples/asyncio/tornado_asyncio.py
+++ examples/asyncio/tornado_asyncio.py
@@ -1,18 +1,23 @@
 import asyncio
+import zmq.asyncio
 from tornado.platform.asyncio import AsyncIOMainLoop
 AsyncIOMainLoop().install()
 
 # This must be instantiated after the installing the IOLoop
 queue = asyncio.Queue()
+ctx = zmq.asyncio.Context()
 
 async def pushing():
+    server = ctx.socket(zmq.PUSH)
+    server.bind('tcp://*:9000')
     while True:
-        await queue.put("hello")
-        await asyncio.sleep(1)
+        await server.send(b"Hello")
 
 async def pulling():
+    client = ctx.socket(zmq.PULL)
+    client.connect('tcp://127.0.0.1:9000')
     while True:
-        greeting = await queue.get()
+        greeting = await client.recv()
         print(greeting)
 
 def zmq_tornado_loop():
