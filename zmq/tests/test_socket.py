f0f346af491588485609be3e8db68877cd1be98d
Min RK <benjaminrk@gmail.com>
2017-02-26 15:41:14 +0100

    add send/recv_serialized
    generic hook for custom serialization functions

--- zmq/tests/test_socket.py
+++ zmq/tests/test_socket.py
@@ -3,6 +3,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 import copy
+import json
 import os
 import platform
 import time
@@ -496,6 +497,39 @@ class TestSocket(BaseZMQTestCase):
         a.send(buf, copy=False)
         rcvd = b.recv()
         assert rcvd == buf
+    
+    def test_custom_serialize(self):
+        a, b = self.create_bound_pair(zmq.DEALER, zmq.ROUTER)
+        def serialize(msg):
+            frames = []
+            frames.extend(msg.get('identities', []))
+            content = json.dumps(msg['content']).encode('utf8')
+            frames.append(content)
+            return frames
+        
+        def deserialize(frames):
+            identities = frames[:-1]
+            content = json.loads(frames[-1].decode('utf8'))
+            return {
+                'identities': identities,
+                'content': content,
+            }
+        
+        msg = {
+            'content': {
+                'a': 5,
+                'b': 'bee',
+            }
+        }
+        a.send_serialized(msg, serialize)
+        recvd = b.recv_serialized(deserialize)
+        assert recvd['content'] == msg['content']
+        assert recvd['identities']
+        # bounce back, tests identities
+        b.send_serialized(recvd, serialize)
+        r2 = a.recv_serialized(deserialize)
+        assert r2['content'] == msg['content']
+        assert not r2['identities']
 
 
 if have_gevent and not windows:
