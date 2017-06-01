50347725566ce92252cf1d754e81171c78d6f57e
Min RK <benjaminrk@gmail.com>
2017-06-01 12:18:07 -0700

    handle removal of ZMQIOLoop in ZMQStream

--- zmq/tests/test_zmqstream.py
+++ zmq/tests/test_zmqstream.py
@@ -6,23 +6,39 @@
 import sys
 import time
 
+from tornado import gen
 from unittest import TestCase
 
 import zmq
 from zmq.eventloop import ioloop, zmqstream
 
 class TestZMQStream(TestCase):
-    
+
     def setUp(self):
         self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.REP)
         self.loop = ioloop.IOLoop.instance()
-        self.stream = zmqstream.ZMQStream(self.socket)
-    
+        self.push = zmqstream.ZMQStream(self.context.socket(zmq.PUSH))
+        self.pull = zmqstream.ZMQStream(self.context.socket(zmq.PULL))
+        port = self.push.bind_to_random_port('tcp://127.0.0.1')
+        self.pull.connect('tcp://127.0.0.1:%i' % port)
+        self.stream = self.push
+
     def tearDown(self):
-        self.socket.close()
+        self.loop.close(all_fds=True)
         self.context.term()
-    
+        ioloop.IOLoop.clear_instance()
+
+    def run_until_timeout(self, timeout=10):
+        timed_out = []
+        @gen.coroutine
+        def sleep_timeout():
+            yield gen.sleep(timeout)
+            timed_out[:] = ['timed out']
+            self.loop.stop()
+        self.loop.add_callback(lambda : sleep_timeout())
+        self.loop.start()
+        assert not timed_out
+
     def test_callable_check(self):
         """Ensure callable check works (py3k)."""
         
@@ -31,4 +47,21 @@ class TestZMQStream(TestCase):
         self.assertRaises(AssertionError, self.stream.on_recv, 1)
         self.assertRaises(AssertionError, self.stream.on_send, 1)
         self.assertRaises(AssertionError, self.stream.on_recv, zmq)
-        
+
+    def test_on_recv_basic(self):
+        sent = [b'basic']
+        def callback(msg):
+            assert msg == sent
+            self.loop.stop()
+        self.loop.add_callback(lambda : self.push.send_multipart(sent))
+        self.pull.on_recv(callback)
+        self.run_until_timeout()
+
+    def test_on_recv_wake(self):
+        sent = [b'wake']
+        def callback(msg):
+            assert msg == sent
+            self.loop.stop()
+        self.pull.on_recv(callback)
+        self.loop.call_later(1, lambda : self.push.send_multipart(sent))
+        self.run_until_timeout()
