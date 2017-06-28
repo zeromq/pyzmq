2f077f7dc7179d26448419c21448d0bda4971e30
Min RK <benjaminrk@gmail.com>
2017-06-28 09:31:56 +0100

    bump version to 17.0.0-dev

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -7,8 +7,8 @@
 from zmq.backend import zmq_version_info
 
 
-VERSION_MAJOR = 16
-VERSION_MINOR = 1
+VERSION_MAJOR = 17
+VERSION_MINOR = 0
 VERSION_PATCH = 0
 VERSION_EXTRA = "dev"
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
