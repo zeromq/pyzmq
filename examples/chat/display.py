baddce10228f4392b50b38a054f6e7b664726601
cclauss <cclauss@bluewin.ch>
2017-09-02 16:54:49 +0200

    print() function

--- examples/chat/display.py
+++ examples/chat/display.py
@@ -26,16 +26,16 @@ def main(addrs):
     socket = context.socket(zmq.SUB)
     socket.setsockopt(zmq.SUBSCRIBE, "")
     for addr in addrs:
-        print "Connecting to: ", addr
+        print("Connecting to: ", addr)
         socket.connect(addr)
 
     while True:
         msg = socket.recv_pyobj()
-        print "%s: %s" % (msg[1], msg[0])
+        print("%s: %s" % (msg[1], msg[0]))
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 2:
-        print "usage: display.py <address> [,<address>...]"
+        print("usage: display.py <address> [,<address>...]")
         raise SystemExit
     main(sys.argv[1:])
