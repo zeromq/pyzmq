9e562e60f7580861af8973773b43b38c34496d08
cclauss <cclauss@bluewin.ch>
2017-09-03 00:33:44 +0200

    from __future__ import print_function

--- examples/chat/display.py
+++ examples/chat/display.py
@@ -18,6 +18,7 @@
 #    You should have received a copy of the Lesser GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
 import zmq
 
 def main(addrs):
