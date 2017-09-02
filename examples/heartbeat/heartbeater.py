6c8043b5a92fe1d9789a8693dc732a7cec264e93
cclauss <cclauss@bluewin.ch>
2017-09-02 17:05:57 +0200

    print() function

--- examples/heartbeat/heartbeater.py
+++ examples/heartbeat/heartbeater.py
@@ -22,55 +22,54 @@ class HeartBeater(object):
     """A basic HeartBeater class
     pingstream: a PUB stream
     pongstream: an ROUTER stream"""
-    
+
     def __init__(self, loop, pingstream, pongstream, period=1000):
         self.loop = loop
         self.period = period
-        
+
         self.pingstream = pingstream
         self.pongstream = pongstream
         self.pongstream.on_recv(self.handle_pong)
-        
+
         self.hearts = set()
         self.responses = set()
         self.lifetime = 0
         self.tic = time.time()
-        
+
         self.caller = ioloop.PeriodicCallback(self.beat, period, self.loop)
         self.caller.start()
-    
+
     def beat(self):
         toc = time.time()
         self.lifetime += toc-self.tic
         self.tic = toc
-        print self.lifetime
+        print(self.lifetime)
         # self.message = str(self.lifetime)
         goodhearts = self.hearts.intersection(self.responses)
         heartfailures = self.hearts.difference(goodhearts)
         newhearts = self.responses.difference(goodhearts)
-        # print newhearts, goodhearts, heartfailures
+        # print(newhearts, goodhearts, heartfailures)
         map(self.handle_new_heart, newhearts)
         map(self.handle_heart_failure, heartfailures)
         self.responses = set()
-        print "%i beating hearts: %s"%(len(self.hearts),self.hearts)
+        print("%i beating hearts: %s"%(len(self.hearts),self.hearts))
         self.pingstream.send(str(self.lifetime))
-    
+
     def handle_new_heart(self, heart):
-        print "yay, got new heart %s!"%heart
+        print("yay, got new heart %s!"%heart)
         self.hearts.add(heart)
-    
+
     def handle_heart_failure(self, heart):
-        print "Heart %s failed :("%heart
+        print("Heart %s failed :("%heart)
         self.hearts.remove(heart)
-        
-    
+
     def handle_pong(self, msg):
         "if heart is beating"
         if msg[1] == str(self.lifetime):
             self.responses.add(msg[0])
         else:
-            print "got bad heartbeat (possibly old?): %s"%msg[1]
-        
+            print("got bad heartbeat (possibly old?): %s"%msg[1])
+
 # sub.setsockopt(zmq.SUBSCRIBE)
 
 
@@ -81,10 +80,10 @@ if __name__ == '__main__':
     pub.bind('tcp://127.0.0.1:5555')
     router = context.socket(zmq.ROUTER)
     router.bind('tcp://127.0.0.1:5556')
-    
+
     outstream = zmqstream.ZMQStream(pub, loop)
     instream = zmqstream.ZMQStream(router, loop)
-    
+
     hb = HeartBeater(loop, outstream, instream)
-    
+
     loop.start()
