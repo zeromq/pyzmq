74b19f6eb69cee2e75d4b1fde58d7cd55738b2a9
Min RK <benjaminrk@gmail.com>
2018-07-30 11:32:20 +0200

    back to dev

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -8,9 +8,9 @@ from zmq.backend import zmq_version_info
 
 
 VERSION_MAJOR = 17
-VERSION_MINOR = 1
+VERSION_MINOR = 2
 VERSION_PATCH = 0
-VERSION_EXTRA = ""
+VERSION_EXTRA = "dev"
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 if VERSION_EXTRA:
