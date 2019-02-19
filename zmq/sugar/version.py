b99ff8d49a80b386ab66f233fe95f356862a0c2a
Min RK <benjaminrk@gmail.com>
2019-02-19 09:28:53 +0100

    release 17.1.3

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -9,7 +9,7 @@ from zmq.backend import zmq_version_info
 
 VERSION_MAJOR = 17
 VERSION_MINOR = 1
-VERSION_PATCH = 2
+VERSION_PATCH = 3
 VERSION_EXTRA = ""
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
