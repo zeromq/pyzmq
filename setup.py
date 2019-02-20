ce0583a5b350b225ba3bdaaedf6dc8816151ef26
Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>
2019-02-20 11:21:20 -0800

    Fix parsing README.md unicode char handling during setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 #-----------------------------------------------------------------------------
 #  Copyright (C) PyZMQ Developers
 #  Distributed under the terms of the Modified BSD License.
@@ -24,6 +26,7 @@ import sys
 import time
 import errno
 import platform
+import codecs
 from traceback import print_exc
 
 # whether any kind of bdist is happening
@@ -1236,8 +1239,7 @@ def find_packages():
 #-----------------------------------------------------------------------------
 # Main setup
 #-----------------------------------------------------------------------------
-
-with open("README.md") as f:
+with codecs.open('README.md', 'r', 'utf-8') as f:
     long_desc = f.read()
 
 setup_args = dict(
