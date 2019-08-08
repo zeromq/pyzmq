00366141341e4bce915ccd2d8721854cceffcf54
Min RK <benjaminrk@gmail.com>
2019-08-08 14:43:50 +0200

    release 18.1.0

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -10,7 +10,7 @@ from zmq.backend import zmq_version_info
 VERSION_MAJOR = 18
 VERSION_MINOR = 1
 VERSION_PATCH = 0
-VERSION_EXTRA = "dev"
+VERSION_EXTRA = ""
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 if VERSION_EXTRA:
