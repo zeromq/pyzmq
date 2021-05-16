b5bb7cb6f4276e31990f4fc5bbe0f792d3409ac4
Min RK <benjaminrk@gmail.com>
2021-05-16 14:13:43 +0200

    import asyncio-proactor support from tornado
    instead of vendoring it.

--- zmq/asyncio.py
+++ zmq/asyncio.py
@@ -21,9 +21,15 @@ from zmq import _future
 _selectors: WeakKeyDictionary = WeakKeyDictionary()
 
 
+class ProactorSelectorThreadWarning(RuntimeWarning):
+    """Warning class for notifying about the extra thread spawned by tornado
+
+    We automatically support proactor via tornado's AddThreadSelectorEventLoop"""
+
+
 def _get_selector_windows(
-    io_loop,
-) -> Union[asyncio.AbstractEventLoop, "_zmq._asyncio_selector.SelectorThread"]:
+    asyncio_loop,
+) -> asyncio.AbstractEventLoop:
     """Get selector-compatible loop
 
     Returns an object with ``add_reader`` family of methods,
@@ -33,18 +39,26 @@ def _get_selector_windows(
     *reader methods, which we need for zmq sockets.
     """
 
-    if io_loop in _selectors:
-        return _selectors[io_loop]
+    if asyncio_loop in _selectors:
+        return _selectors[asyncio_loop]
 
     # detect add_reader instead of checking for proactor?
     if hasattr(asyncio, "ProactorEventLoop") and isinstance(
-        io_loop, asyncio.ProactorEventLoop  # type: ignore
+        asyncio_loop, asyncio.ProactorEventLoop  # type: ignore
     ):
-        from ._asyncio_selector import SelectorThread
+        try:
+            from tornado.platform.asyncio import AddThreadSelectorEventLoop
+        except ImportError:
+            raise RuntimeError(
+                "Proactor event loop does not implement add_reader family of methods required for zmq."
+                " zmq will work with proactor if tornado >= 6.1 can be found."
+                " Use `asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())`"
+                " or install 'tornado>=6.1' to avoid this error."
+            )
 
         warnings.warn(
             "Proactor event loop does not implement add_reader family of methods required for zmq."
-            " Registering an additional selector thread for add_reader support."
+            " Registering an additional selector thread for add_reader support via tornado."
             " Use `asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())`"
             " to avoid this warning.",
             RuntimeWarning,
@@ -52,20 +66,22 @@ def _get_selector_windows(
             stacklevel=5,
         )
 
-        selector = _selectors[io_loop] = SelectorThread(io_loop)
+        selector_loop = _selectors[asyncio_loop] = AddThreadSelectorEventLoop(asyncio_loop)  # type: ignore
 
         # patch loop.close to also close the selector thread
-        loop_close = io_loop.close
+        loop_close = asyncio_loop.close
 
         def _close_selector_and_loop():
-            _selectors.pop(io_loop, None)
-            selector.close()
-            loop_close()
+            # restore original before calling selector.close,
+            # which in turn calls eventloop.close!
+            asyncio_loop.close = loop_close
+            _selectors.pop(asyncio_loop, None)
+            selector_loop.close()
 
-        io_loop.close = _close_selector_and_loop
-        return selector
+        asyncio_loop.close = _close_selector_and_loop
+        return selector_loop
     else:
-        return io_loop
+        return asyncio_loop
 
 
 def _get_selector_noop(loop) -> asyncio.AbstractEventLoop:
