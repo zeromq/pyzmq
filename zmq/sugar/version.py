bfcacc951ffbcb17d22afc61674d773e27cf1b93
Min RK <benjaminrk@gmail.com>
2021-05-26 13:58:47 +0200

    back to dev

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -3,15 +3,15 @@
 # Copyright (C) PyZMQ Developers
 # Distributed under the terms of the Modified BSD License.
 
-from typing import Optional, Tuple, Union
+from typing import Tuple, Union
 
 from zmq.backend import zmq_version_info
 
 
 VERSION_MAJOR = 22
-VERSION_MINOR = 1
+VERSION_MINOR = 2
 VERSION_PATCH = 0
-VERSION_EXTRA = ""
+VERSION_EXTRA = "dev"
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 version_info: Union[Tuple[int, int, int], Tuple[int, int, int, float]] = (
