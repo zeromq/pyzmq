1fe7436c86f9d6285b9b4479992e5068deffc6fc
Min RK <benjaminrk@gmail.com>
2021-05-26 13:57:59 +0200

    release 22.1.0

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -9,8 +9,8 @@ from zmq.backend import zmq_version_info
 
 
 VERSION_MAJOR = 22
-VERSION_MINOR = 0
-VERSION_PATCH = 3
+VERSION_MINOR = 1
+VERSION_PATCH = 0
 VERSION_EXTRA = ""
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
