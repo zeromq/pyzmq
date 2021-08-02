966de3426e7cf99d9e17dc87280f191d42c7bb81
Min RK <benjaminrk@gmail.com>
2021-08-02 09:24:46 +0200

    only assert has(ipc) on bundled libzmq

--- zmq/tests/test_etc.py
+++ zmq/tests/test_etc.py
@@ -8,14 +8,21 @@ import zmq
 from pytest import mark
 
 
-@mark.skipif('zmq.zmq_version_info() < (4,3)')
+only_bundled = mark.skipif(not hasattr(zmq, '_libzmq'), reason="bundled libzmq")
+
+
+@mark.skipif('zmq.zmq_version_info() < (4, 1)')
 def test_has():
     assert not zmq.has('something weird')
-    has_ipc = zmq.has('ipc')
-    assert has_ipc == sys.platform.startswith('win')
 
 
-@mark.skipif(not hasattr(zmq, '_libzmq'), reason="bundled libzmq")
+@only_bundled
 def test_has_curve():
     """bundled libzmq has curve support"""
     assert zmq.has('curve')
+
+
+@only_bundled
+def test_has_ipc():
+    """bundled libzmq has ipc support"""
+    assert zmq.has('ipc')
