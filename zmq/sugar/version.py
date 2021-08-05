63ed17b92e704e9e5f6dc7688e129e689e5bf8e2
Min RK <benjaminrk@gmail.com>
2021-08-05 10:25:54 +0200

    release 22.2.1

--- zmq/sugar/version.py
+++ zmq/sugar/version.py
@@ -9,9 +9,9 @@ from zmq.backend import zmq_version_info
 
 
 VERSION_MAJOR = 22
-VERSION_MINOR = 3
-VERSION_PATCH = 0
-VERSION_EXTRA = "dev"
+VERSION_MINOR = 2
+VERSION_PATCH = 1
+VERSION_EXTRA = ""
 __version__ = '%i.%i.%i' % (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 
 version_info: Union[Tuple[int, int, int], Tuple[int, int, int, float]] = (
