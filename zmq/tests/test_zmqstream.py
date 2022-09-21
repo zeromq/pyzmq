c5dfe96264ca17197316e26aa8c38aa8bc783809
Min RK <benjaminrk@gmail.com>
2022-09-21 10:29:11 +0200

    tornado 4 compatibility in test timeouts

--- zmq/tests/test_zmqstream.py
+++ zmq/tests/test_zmqstream.py
@@ -3,6 +3,7 @@
 
 
 import asyncio
+from functools import partial
 from unittest import TestCase
 
 import pytest
@@ -11,7 +12,7 @@ import zmq
 
 try:
     import tornado
-    from tornado import ioloop
+    from tornado import gen, ioloop
 
     from zmq.eventloop import zmqstream
 except ImportError:
@@ -24,6 +25,8 @@ class TestZMQStream(TestCase):
             pytest.skip()
         self.context = zmq.Context()
         self.loop = ioloop.IOLoop(make_current=False)
+        if tornado and tornado.version_info < (5,):
+            self.loop.make_current()
 
         async def _make_sockets():
             self.push = zmqstream.ZMQStream(self.context.socket(zmq.PUSH))
@@ -38,7 +41,11 @@ class TestZMQStream(TestCase):
     def tearDown(self):
         if self._timeout_task:
             self._timeout_task.cancel()
-            self.loop.run_sync(lambda: asyncio.wait_for(self._timeout_task, timeout=10))
+
+            async def wait():
+                await gen.with_timeout(self.loop.time() + 1, self._timeout_task)
+
+            self.loop.run_sync(wait)
         self.loop.close(all_fds=True)
         self.context.term()
 
@@ -47,14 +54,17 @@ class TestZMQStream(TestCase):
 
         async def sleep_timeout():
             try:
-                await asyncio.sleep(timeout)
+                await gen.sleep(timeout)
             except asyncio.CancelledError:
                 return
             timed_out[:] = ['timed out']
             self.loop.stop()
 
         def make_timeout_task():
-            self._timeout_task = asyncio.ensure_future(sleep_timeout())
+            if tornado.version_info < (5,):
+                self.loop.add_callback(sleep_timeout)
+            else:
+                self._timeout_task = asyncio.ensure_future(sleep_timeout())
 
         self.loop.run_sync(make_timeout_task)
         self.loop.start()
@@ -76,8 +86,9 @@ class TestZMQStream(TestCase):
             assert msg == sent
             self.loop.stop()
 
-        self.loop.add_callback(lambda: self.push.send_multipart(sent))
-        self.loop.add_callback(lambda: self.pull.on_recv(callback))
+        self.loop.run_sync(partial(self.push.send_multipart, sent))
+        self.loop.call_later(1, lambda: self.pull.on_recv(callback))
+        self.pull.on_recv(callback)
         self.run_until_timeout()
 
     def test_on_recv_wake(self):
