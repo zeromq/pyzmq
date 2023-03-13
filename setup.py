bd45c09279a3933a627ace96db25598a3f7f9f7a
Min RK <benjaminrk@gmail.com>
2023-03-13 10:36:48 +0100

    Bump to 25.0.1

--- setup.py
+++ setup.py
@@ -1322,7 +1322,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup_args = dict(
     name="pyzmq",
-    version="25.1.0.dev",
+    version="25.0.1",
     packages=find_packages(),
     ext_modules=extensions,
     cffi_modules=cffi_modules,
