2b489ff604701fad6cde1a1df817398f0c4a8bee
Min RK <benjaminrk@gmail.com>
2024-11-27 13:02:47 +0100

    fix handling of tornado Apache license
    - clarify correct license for the derivative work
    - redistribute apache license file

--- zmq/eventloop/zmqstream.py
+++ zmq/eventloop/zmqstream.py
@@ -1,17 +1,8 @@
+# Derived from iostream.py from tornado 1.0, Copyright 2009 Facebook
+# Used under Apache License Version 2.0
 #
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# Modifications are Copyright (C) PyZMQ Developers
+# Distributed under the terms of the Modified BSD License.
 """A utility class for event-based messaging on a zmq socket using tornado.
 
 .. seealso::
